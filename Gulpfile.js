ddc9090eaa00bf6c3d1048fd8084a36cb4518e9e
Cesar Boaventura <cesar.boaventura61@gmail.com>
2016-06-11 19:01:58 -0300

    Remove Jade and add Pug, add app folder for Pug and Stylus

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,60 +3,79 @@ var url = 'yoururl.app'; // Local dev URL. Change as needed.
 
 // Load plugins
 var 
-	gulp         = require('gulp'),
-	stylus       = require('gulp-stylus'),
-	autoprefixer = require('autoprefixer'),
-	lost         = require('lost'),
-	poststylus   = require('poststylus'),
-	rucksack     = require('rucksack-css'),
-	typographic  = require('typographic'),
-	axis         = require('axis'),
-	rupture      = require('rupture'),
-	minifyCss    = require('gulp-minify-css'),
-  rename       = require('gulp-rename'),
-	plumber      = require('plumber'),
-	browserSync  = require('browser-sync').create()
+gulp         = require('gulp'),
+stylus       = require('gulp-stylus'),
+pug          = require('gulp-pug'),
+autoprefixer = require('autoprefixer'),
+lost         = require('lost'),
+poststylus   = require('poststylus'),
+rucksack     = require('rucksack-css'),
+typographic  = require('typographic'),
+axis         = require('axis'),
+rupture      = require('rupture'),
+minifyCss    = require('gulp-minify-css'),
+rename       = require('gulp-rename'),
+plumber      = require('plumber'),
+browserSync  = require('browser-sync').create()
 
 
 
 //- Load Libs for Stylus
 var stylus_options = {
-    use: [poststylus([
-        'lost', 
-        'rucksack-css', 
-        autoprefixer({browsers: ['last 2 versions', 'ie 8', 'ie 9', '> 1%']})]),
-        rupture(),
-        axis(),
-        typographic()]
+  use: [poststylus([
+    'lost', 
+    'rucksack-css', 
+    autoprefixer({browsers: ['last 2 versions', 'ie 8', 'ie 9', '> 1%']})]),
+  rupture(),
+  axis(),
+  typographic()]
 };
 
-// Styles
+
 gulp.task('styles', function(){
-	return gulp.src('./styl/*.styl') // Two files get compiled here: main stylsheet (all partials imported) and editor stylesheet. Makes for simple gulpfile config, but maybe not best approach. Comments welcome! 
- 	.pipe(plumber())
+  return gulp.src('./app/stylus/*.styl') // Two files get compiled here: main stylsheet (all partials imported) and editor stylesheet. Makes for simple gulpfile config, but maybe not best approach. Comments welcome! 
   .pipe(stylus(stylus_options))
-	.pipe(gulp.dest('./'))
-	.pipe(rename({ suffix: '.min' }))
-	.pipe(minifyCss())
-	.pipe(gulp.dest('./'))
-  .pipe(browserSync.stream());
+  .pipe(gulp.dest('./public/css'))
+  .pipe(browserSync.stream())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(minifyCss())
+  .pipe(gulp.dest('./public/css'))
+  
+});
+
+gulp.task('pug', function buildHTML() {
+  return gulp.src('./app/views/*.pug')
+  .pipe(pug())
+  .pipe(gulp.dest('./public'))
 });
 
 gulp.task('js', function() {
-  return gulp.src('./assets/js/main.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./assets/js/min'));
+  return gulp.src('./app/js/main.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./public/js/min'));
 });
 
+/* If you want proxy */
 
-gulp.task('browser-sync', function(){
-	browserSync.init({
-        proxy: url,
-        notify: false
-    });
+// gulp.task('browser-sync', function(){
+//   browserSync.init({
+//         // proxy: url,
+//         notify: false,
+//         baseDir: "./public"
+//     });
+// });
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./public"
+    }
+  });
 });
 
 // Watch!
-gulp.task('watch',['browser-sync'], function(){
-	gulp.watch('./styl/**/*.styl', ['styles']);
+gulp.task('default',['browser-sync', 'styles', 'pug'], function(){
+  gulp.watch('./app/stylus/**/*.styl', ['styles']);
+  gulp.watch('./app/views/**/*.pug', ['pug']);
+  gulp.watch("./public/*.html").on('change', browserSync.reload);
 });
